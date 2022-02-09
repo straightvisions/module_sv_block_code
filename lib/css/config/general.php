192a14d4a387008587be05661b8a4b2f0d5b4568
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-02-09 21:23:09 +0100

    FIX CSS

--- lib/css/config/general.php
+++ lib/css/config/general.php
@@ -1,6 +1,6 @@
 <?php
 	echo $_s->build_css(
-		is_admin() ? '.editor-styles-wrapper .wp-block-quote' : '.wp-block-quote',
+		is_admin() ? '.editor-styles-wrapper .wp-block-code' : '.wp-block-code',
 		array_merge(
 			$module->get_setting('font')->get_css_data('font-family'),
 			$module->get_setting('font_size')->get_css_data('font-size','','px'),
@@ -11,13 +11,3 @@
 			$module->get_setting('border')->get_css_data()
 		)
 	);
-
-echo $_s->build_css(
-	is_admin() ? '.editor-styles-wrapper .wp-block-quote p' : '.wp-block-quote p',
-	array_merge(
-		$module->get_setting('font')->get_css_data('font-family'),
-		$module->get_setting('font_size')->get_css_data('font-size','','px'),
-		$module->get_setting('line_height')->get_css_data('line-height'),
-		$module->get_setting('text_color')->get_css_data(),
-	)
-);
