bb041710d6d5b3751754c3ed6edc28a9bf70fa49
Matthias Bathke <matthias-bathke@straightvisions.com>
2022-06-20 20:04:03 +0200

    support for site editor templates

--- sv_block_code.php
+++ sv_block_code.php
@@ -11,6 +11,7 @@
 				->set_section_template_path()
 				->set_section_order(5000)
 				->set_section_icon('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22 6v16h-20v-16h20zm2-6h-24v24h24v-24zm-11 11v1.649l3.229 1.351-3.229 1.347v1.653l5-2.201v-1.599l-5-2.2zm-7 2.201v1.599l5 2.2v-1.653l-3.229-1.347 3.229-1.351v-1.649l-5 2.201z"/></svg>')
+				->set_block_handle('wp-block-code')
 				->get_root()
 				->add_section( $this );
 		}
